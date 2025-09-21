7882ae419bee06a2f18d1455053f2b8ebb8e190c
ThreeFrogs511 <nicolas.lavarde@gmail.com>
2025-09-21 14:13:57 +0100

    cleaning 2.0

--- main.js
+++
@@ -1,6 +0,0 @@
-import { memoChamp } from "./memochamp.js";
-
-//Fonction principale qui démarre le jeu
-const section = document.querySelector(".jeu");
-memoChamp(section)
-;
--- memochamp.js
+++
@@ -1,7 +0,0 @@
-import { presentationNiveau, commencerJeuOuAnnuler } from "./function.js";
-
-//La fonction principale qui démarre tout le processus
-export async function memoChamp(section) {
-    let difficulte = await presentationNiveau(section); // difficulte = n càd le nb de carré
-    commencerJeuOuAnnuler(section, difficulte)
-    }
